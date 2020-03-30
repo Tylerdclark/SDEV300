baacd9b33148fbcf66f1da498cb99f5fb46170d4
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-03-30 00:05:43 -0400

    documentation and several changes

--- wk2/math_functions.py
+++ wk2/math_functions.py
@@ -4,50 +4,54 @@ from math import sin, cos, pi, sqrt, log10
 
 
 def sin_output(start, stop, increment):
-    """"""
-    print('input / sin output')
+    """Generates values of sin over a given range with given increment """
+    print('sin output:')
+    print('x, f(x)')
     while stop >= start:
         x_value = start
         y_value = sin(start)
-        print(f'{x_value:.2f}  {y_value:.2f}')
+        print(f'{x_value:.2f}, {y_value:.2f}')  # to standard output
         start = x_value + increment
 
 
 def cos_output(start, stop, increment):
-    print('input / cos output')
+    """Generates values of cos over a given range with given increment """
+    print('cos output:')
+    print('x, f(x)')
     while stop >= start:
         x_value = start
         y_value = cos(start)
-        print(f'{start:.2f}  {y_value:.2f}')
+        print(f'{start:.2f}, {y_value:.2f}')  # to standard output
         start = x_value + increment
 
 
 def sqrt_output(start, stop, increment):
-    print('input / sqrt output')
+    """Generates values of sqrt over a given range with given increment """
+    print('sqrt output:')
+    print('x, f(x)')
     while stop >= start:
         x_value = start
         y_value = sqrt(start)
-        print(f'{start:.2f}  {y_value:.2f}')
+        print(f'{start:.2f}, {y_value:.2f}')  # to standard output
         start = x_value + increment
 
 
 def log10_output(start, stop, increment):
-    print('input / log10 output')
+    """Generates values of log10 over a given range with given increment """
+    print('log10 output:')
+    print('x, f(x)')
     while stop >= start:
         if start == 0:
-            print('0 undefined')
+            print('0, undefined')
             start += increment
         else:
             x_value = start
             y_value = log10(start)
-            print(f'{start:.2f}  {y_value:.2f}')
+            print(f'{start:.2f}, {y_value:.2f}')  # to standard output
             start = x_value + increment
 
 
-# sin_output(-2 * pi, 2 * pi, pi / 64)
-# cos_output(-2 * pi, 2 * pi, pi / 64)
+sin_output(-2 * pi, 2 * pi, pi / 64)
+cos_output(-2 * pi, 2 * pi, pi / 64)
+sqrt_output(0, 200, 0.5)
 log10_output(0, 200, 0.5)
-# file_writer = open("math_output2.txt", "w")
-# for i in range(10):
-#     file_writer.write("This is line %d\r\n" % (i + 1))
-# file_writer.close()
