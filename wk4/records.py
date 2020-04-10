5ae951140bd56ee5078f8b1d97c64c3d00bc7cd7
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-09 20:32:48 -0400

    Made a bunch of functions. Will likely need to come back

--- wk4/records.py
+++ wk4/records.py
@@ -1,10 +1,46 @@
 # records.py
 """formats address records and eliminates records with missing critical fields."""
-import numpy as np
+import re
 import pandas as pd
 
-# record_data = np.genfromtxt(r'/Users/tylerclark/Repos/SDEV300/wk4/MOCK_DATA.csv', delimiter=',', skip_header=1,usecols=[0,1,2,3],dtype='U')
+df = pd.read_csv('../wk4/MOCK_DATA.csv', sep=',', header=0)
 
-df = pd.read_csv(r'/Users/tylerclark/Repos/SDEV300/wk4/MOCK_DATA.csv', sep=',',header=None)
-print(df.values)
 
+def get_formatted_phone(value):
+    """using regular expression fullmatch, tries to apply the phone number format or ignores.
+    accepted input phone numbers will be xxxxxxxxxx or xxx-xxx-xxxx"""
+    phone = re.fullmatch(r'(\d{3})(\d{3})(\d{4})', value)
+    normal = re.fullmatch(r'\d{3}-\d{3}-\d{4}', value)
+    if phone:
+        return '-'.join(phone.groups())
+    elif normal:
+        return normal.string
+    else:
+        return ''
+
+
+def get_formatted_zip(value):
+    """using regular expression fullmatch, tries to apply the zip code format or ignores
+    accepted input zip codes will be xxxxx or xxxxxxxxx"""
+    zip_four = re.fullmatch(r'(\d{5})(\d{4})', value)
+    normal_zip = re.fullmatch(r'(\d{5})', value)
+    if zip_four:
+        return '-'.join(zip_four.groups())
+    elif normal_zip:
+        return normal_zip.string
+    else:
+        return ''
+
+
+def format_data(data):
+    """Formats the data frame passed as an argument by calling the other formatting methods"""
+    data.columns = ['First Name', 'Last Name', 'Zip Code', 'Phone Number']
+    data.fillna('', inplace=True) # anything with NaN will be filled with blank string
+    formatted_phone = data['Phone Number'].map(get_formatted_phone)
+    data['Phone Number'] = formatted_phone
+    formatted_zip = data['Zip Code'].map(get_formatted_zip)
+    data['Zip Code'] = formatted_zip
+
+
+format_data(df)
+print(df.to_string(index=False))
