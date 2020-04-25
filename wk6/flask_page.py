5178bf0a97522e03aee0788834fe89e003e5a78c
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-25 12:45:19 -0400

    Pass in time

--- wk6/flask_page.py
+++ wk6/flask_page.py
@@ -1,11 +1,16 @@
+# my_webpage.py
+"""A website using flask and HTML and css templates"""
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+now = datetime.now()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=now.strftime("%m/%d/%Y, %H:%M:%S"))
 
 
 if __name__ == '__main__':
