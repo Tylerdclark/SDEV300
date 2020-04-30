2536924de77836f8362e5c65ce6406b873862533
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-29 23:18:54 -0400

    Accommodating about

--- wk6/flask_page.py
+++ wk6/flask_page.py
@@ -47,5 +47,10 @@ def index():
                            leader_board=leader_board)
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
